3d03274e48f40ec3b19d45d965d251f8de2e9846
jay <jay@contensive.com>
2018-10-24 12:31:46 -0400

    change assembly load failure to info, not warn

--- source/Processor/Controllers/addonController.cs
+++ source/Processor/Controllers/addonController.cs
@@ -1768,7 +1768,7 @@ namespace Contensive.Processor.Controllers {
                                 testAssembly = System.Reflection.Assembly.LoadFrom(TestFilePathname);
                                 //testAssemblyName = testAssembly.FullName
                             } catch (Exception ex) {
-                                LogController.logWarn(core, "Assembly.LoadFrom failure, adding DLL [" + TestFilePathname + "] to assemblySkipList, ex [" + ex.Message + "]");
+                                LogController.logInfo(core, "Assembly.LoadFrom failure, adding DLL [" + TestFilePathname + "] to assemblySkipList, ex [" + ex.Message + "]");
                                 core.assemblySkipList.Add(TestFilePathname);
                                 testFileIsValidAddonAssembly = false;
                             }
